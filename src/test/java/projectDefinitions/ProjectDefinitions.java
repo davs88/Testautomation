1dac38271c335551f9b214c9a19e5ddad39b9e23
davs88 <david.svanhammar@hotmail.com>
2021-04-01 15:11:10 +0200

    Third commit

--- src/test/java/projectDefinitions/ProjectDefinitions.java
+++ src/test/java/projectDefinitions/ProjectDefinitions.java
@@ -4,6 +4,9 @@ package projectDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,67 +17,92 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.rmi.UnexpectedException;
 import java.util.Random;
 
+
 public class ProjectDefinitions {
 
+    Random randomGenerator = new Random();
+    int randomInt = randomGenerator.nextInt(100000);
     private WebDriver driver;
 
+
     @Given("I have entered my {string}")
-    public void i_have_entered_my_email(String string) throws UnexpectedException {
+    public void i_have_entered_my_email(String mail) throws UnexpectedException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://login.mailchimp.com/signup/");
         driver.manage().window().maximize();
-       // WebElement mailfield = driver.findElement(By.id("email"));
-        //mailfield.sendKeys(string);
-        sendKeys(driver, By.id("email"),(string));
+
+        if (mail.equals("mail")) {
+            sendKeys(driver, By.id("email"), mail + randomInt + "@sklm.se");
+        } else if (mail.equals("")) {
+            sendKeys(driver, By.id("email"), (""));
+        }
 
     }
 
     @Given("I have also entered a {string}")
-    public void i_have_also_entered_a_username(String string)throws UnexpectedException {
-        Random randomGenerator = new Random();
-        int randomInt = randomGenerator.nextInt(10000);
-       // WebElement user = driver.findElement(By.id("new_username"));
-        //user.sendKeys(String.valueOf(randomInt));
-        sendKeys(driver,By.id("new_username"),(String.valueOf(randomInt)));
+    public void i_have_also_entered_a_username(String username) throws UnexpectedException {
 
 
+        if (username.equals("username")) {
+            sendKeys(driver, By.id("new_username"), ("xavier" + randomInt));
+
+        } else if (username.equals("Longname")) {
+            sendKeys(driver, By.id("new_username"), (String.valueOf(username.repeat(20))));
+        } else if (username.equals("Dav88999")) {
+            sendKeys(driver, By.id("new_username"), username);
+        } else if (username.equals("username2")) {
+            sendKeys(driver, By.id("new_username"), username + randomInt);
+        }
     }
+
 
     @Given("I have also selected a {string}")
     public void i_have_also_selected_a_password(String string) throws UnexpectedException, InterruptedException {
-        //WebElement password = driver.findElement(By.id("new_password"));
-        //password.sendKeys(string);
-        //Thread.sleep(2000);
-        sendKeys(driver,By.id("new_password"),(string));
 
-        click(driver,By.id("onetrust-accept-btn-handler"));
+        sendKeys(driver, By.id("new_password"), (string));
+
+        click(driver, By.id("onetrust-accept-btn-handler"));
 
     }
 
     @When("I press sign up")
     public void i_press_sign_up() {
-        WebElement signup = driver.findElement(By.cssSelector("#create-account"));
-        signup.click();
 
-        click(driver,By.cssSelector("#create-account"));
-        driver.quit();
-    }
 
-    @Then("the {string} should be on the screen")
-    public void the_result_should_be_on_the_screen(String string) {
+        click(driver, By.cssSelector("#create-account"));
 
     }
 
-
-private void sendKeys(WebDriver driver, By by, String keys) {
-        (new WebDriverWait(driver, 30)).until(ExpectedConditions.presenceOfElementLocated(by));
-        driver.findElement(by).sendKeys(keys);
+    @Then("the {string} should be on the screen for {string}")
+    public void the_result_should_be_on_the_screen(String string, String string2) {
+        if (string2.equals("username")) {
+            WebElement verify = driver.findElement(By.cssSelector("#signup-content > div > div > div > h1"));
+            assertEquals(string, verify.getText());
+        } else if (string2.equals("Longname")) {
+            WebElement check = driver.findElement(By.className("invalid-error"));
+            assertEquals(string, check.getText());
+        } else if (string2.equals("Dav8899")) {
+            WebElement verify = driver.findElement(By.className("invalid-error"));
+            assertEquals(string, verify.getText());
+        } else if (string2.equals("username2")) {
+            WebElement verify = driver.findElement(By.className("invalid-error"));
+            assertEquals(string, verify.getText());
         }
-private void click(WebDriver driver, By by) {
-        (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(by));
+        driver.quit();
+
+    }
+
+
+    private void sendKeys(WebDriver driver, By by, String keys) {
+        (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(by));
+        driver.findElement(by).sendKeys(keys);
+    }
+
+    private void click(WebDriver driver, By by) {
+        (new WebDriverWait(driver, 5)).until(ExpectedConditions.elementToBeClickable(by));
         driver.findElement(by).click();
 
-        }
-        }
+    }
+}
--- target/test-classes/Features/Mailchimp.feature
+++ target/test-classes/Features/Mailchimp.feature
@@ -5,11 +5,14 @@ Feature: Mail Chimp test
 
 
     Scenario Outline: Sign up account
-      Given I have entered my "<email>"
+      Given I have entered my "<mailadress>"
       Given I have also entered a "<username>"
       And I have also selected a "<password>"
       When I press sign up
-      Then the "<result>" should be on the screen
+      Then the "<result>" should be on the screen for "<username>"
       Examples:
-        | email |username|password|result|
-        | mars@mars.se|marsmonth123|Marsmonth1234!|result|
+        | mailadress |username|password|result|
+        |mail|username|Losenord123!|   Check your email|
+        | mail|Longname|Losenord123!|  Enter a value less than 100 characters long|
+        |mail |      Dav88999  |          Losenord123!  |        Another user with this username already exists. Maybe it's your evil twin. Spooky.|
+        |     |    username2    |       Losenord123!     |         Please enter a value|
